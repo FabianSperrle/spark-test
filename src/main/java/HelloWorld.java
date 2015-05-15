084c06ea62968423abf7db488ed3907e7805d25a
Fabian Sperrle <fabian.sperrle@uni.kn>
2015-05-15 11:59:12 +0200

    Simple routes and filters

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,20 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        before("test/:name", (req, res) -> {
+            String user = req.params(":name");
+            if (user.equals("peter")) {
+                throw new Exception("Unknown name");
+            }
+            else if (!user.equals("fabian")) {
+                halt(401, "I don't want you here");
+            }
+        });
 
+        get("/", (req, res) -> "Welcome!");
+        get("/hello", (req, res) -> "Hello World");
         get("/test/:name", (req, res) -> "Hallo " + req.params(":name"));
+
+        exception(Exception.class, (e, request, response) -> response.body("I'm sorry Peter, but not for you."));
     }
 }
